6f59d6ac2d4ea5a4b932a13b24ed653d1a306a91
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2020-09-27 12:44:14 +0000

    Fix settings.

--- app/src/androidTest/java/com/engineerskasa/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/engineerskasa/crypto/ExampleInstrumentedTest.kt
@@ -9,4 +9,6 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute 
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](
