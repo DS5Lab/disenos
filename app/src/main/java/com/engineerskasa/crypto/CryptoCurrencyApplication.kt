f71b39761cd79a3974a09e954d8418fc6900fba2
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2020-10-07 19:35:21 +0000

    Cleaning up the code

--- app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
+++ app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
@@ -2,4 +2,5 @@ package com.engineerskasa.crypto
 
 import android.app.Application
 import com.engineerskasa.crypto.di.component.DaggerAppComponent
-import com.engineerskasa.crypto.d
+import com.engineerskasa.crypto.di.modules.AppModule
+import com.engineerskasa.crypto.di.m
