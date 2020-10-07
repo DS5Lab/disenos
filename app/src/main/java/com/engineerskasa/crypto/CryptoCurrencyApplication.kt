549b9b719638654c3cb90d5605bfadb8efcc8c21
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2020-10-07 19:43:55 +0000

    Adding License file

--- app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
+++ app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
@@ -3,4 +3,5 @@ package com.engineerskasa.crypto
 import android.app.Application
 import com.engineerskasa.crypto.di.component.DaggerAppComponent
 import com.engineerskasa.crypto.di.modules.AppModule
-import com.engineerskasa.crypto.di.m
+import com.engineerskasa.crypto.di.modules.NetModule
+import com.engineerskasa.crypto.utils.Constan
