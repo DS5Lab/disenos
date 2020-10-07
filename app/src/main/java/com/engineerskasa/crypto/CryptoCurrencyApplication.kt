9a4a3d9f56a593416bee0fc1ae2440a2bf1b3004
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2020-10-07 19:27:49 +0000

    актуализация компонентов

--- app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
+++ app/src/main/java/com/engineerskasa/crypto/CryptoCurrencyApplication.kt
@@ -1,4 +1,5 @@
 package com.engineerskasa.crypto
 
 import android.app.Application
-import com.engineerskasa.crypto.di.
+import com.engineerskasa.crypto.di.component.DaggerAppComponent
+import com.engineerskasa.crypto.d
