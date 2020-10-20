67006a9f9c43613ec49307b0d2fce09f0604e920
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2020-10-20 22:30:43 +0000

    Updated read me.

--- app/src/main/java/com/engineerskasa/crypto/MainActivity.kt
+++ app/src/main/java/com/engineerskasa/crypto/MainActivity.kt
@@ -3,4 +3,6 @@ package com.engineerskasa.crypto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import and
+import android.view.View
+import android.widget.Toast
+import androidx.datab
