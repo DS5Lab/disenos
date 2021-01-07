c25daae8bd8e15c981298d3a9228a7b510b94549
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2021-01-07 09:07:41 +0000

    Fix key

--- app/src/main/java/com/engineerskasa/crypto/adapter/CryptoCurrencyAdapter.kt
+++ app/src/main/java/com/engineerskasa/crypto/adapter/CryptoCurrencyAdapter.kt
@@ -3,4 +3,5 @@ package com.engineerskasa.crypto.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.T
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerVie
