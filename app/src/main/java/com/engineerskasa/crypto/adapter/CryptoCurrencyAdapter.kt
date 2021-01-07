b5aa51ba00da3ff451be7a45592dcdaefef50e4d
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2021-01-07 14:46:33 +0000

    git am: ignore dirty submodules

--- app/src/main/java/com/engineerskasa/crypto/adapter/CryptoCurrencyAdapter.kt
+++ app/src/main/java/com/engineerskasa/crypto/adapter/CryptoCurrencyAdapter.kt
@@ -4,4 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerVie
+import androidx.recyclerview.widget.RecyclerView
+import com.engineerskasa.crypto.R
+import com.
