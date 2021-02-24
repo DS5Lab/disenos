f55bcd08816f8930155d0f97eda9a20decf3f798
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2021-02-24 14:08:25 +0000

    Reviewed by John.

--- app/src/main/java/com/engineerskasa/crypto/data/remote/ApiInterface.kt
+++ app/src/main/java/com/engineerskasa/crypto/data/remote/ApiInterface.kt
@@ -3,4 +3,7 @@ package com.engineerskasa.crypto.data.remote
 import com.engineerskasa.crypto.model.CryptoCurrency
 import io.reactivex.Observable
 import retrofit2.http.GET
-import retrofit
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @G
