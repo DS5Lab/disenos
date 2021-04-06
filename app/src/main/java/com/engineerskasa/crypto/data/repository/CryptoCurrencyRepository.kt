3f48b9a591eb487dd2c44a44866080264c47b2de
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2021-04-06 15:06:25 +0000

    update to new API

--- app/src/main/java/com/engineerskasa/crypto/data/repository/CryptoCurrencyRepository.kt
+++ app/src/main/java/com/engineerskasa/crypto/data/repository/CryptoCurrencyRepository.kt
@@ -1,4 +1,5 @@
 package com.engineerskasa.crypto.data.repository
 
 import android.util.Log
-import com.engineerskasa.
+import com.engineerskasa.crypto.data.database.CryptoDao
+import com.engineerskasa
