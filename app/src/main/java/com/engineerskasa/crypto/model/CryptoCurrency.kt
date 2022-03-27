cca90accf282a375eba592dca09bca57b05bd87a
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-03-27 09:38:34 +0000

    [DOCU] German:

--- app/src/main/java/com/engineerskasa/crypto/model/CryptoCurrency.kt
+++ app/src/main/java/com/engineerskasa/crypto/model/CryptoCurrency.kt
@@ -2,4 +2,7 @@ package com.engineerskasa.crypto.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.ann
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+@E
