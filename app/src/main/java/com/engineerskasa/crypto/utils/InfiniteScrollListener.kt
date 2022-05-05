2fa4669a9d2b3a35bfa6b8e71ec0593c552ef7a9
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-05-05 09:39:04 +0000

    + ‘tidal/.#newvis.hs’

--- app/src/main/java/com/engineerskasa/crypto/utils/InfiniteScrollListener.kt
+++ app/src/main/java/com/engineerskasa/crypto/utils/InfiniteScrollListener.kt
@@ -3,4 +3,5 @@ package com.engineerskasa.crypto.utils
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class InfiniteScrollListener( val func: () -> Unit,  val layoutManager: LinearL
+class InfiniteScrollListener( val func: () -> Unit,  val layoutManager: LinearLayoutManager): RecyclerView.OnScrollListener() {
+    
