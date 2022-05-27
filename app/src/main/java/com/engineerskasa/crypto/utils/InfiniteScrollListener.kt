2285c4567364786309948b31f75b2f46de1c9624
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-05-27 09:07:30 +0000

    Docs update

--- app/src/main/java/com/engineerskasa/crypto/utils/InfiniteScrollListener.kt
+++ app/src/main/java/com/engineerskasa/crypto/utils/InfiniteScrollListener.kt
@@ -14,4 +14,6 @@ class InfiniteScrollListener( val func: () -> Unit,  val layoutManager: LinearLa
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
         if (dy > 0) {
-            visibleItemCount = recyclerView.
+            visibleItemCount = recyclerView.childCount
+            totalItemCount = layoutManager.itemCount
+ 
