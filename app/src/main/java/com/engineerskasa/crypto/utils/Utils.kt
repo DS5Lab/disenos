21cc281d023065004b5ad1dcf9ef15f143cd3fa7
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-07-27 16:14:57 +0000

    Code restructuring

--- app/src/main/java/com/engineerskasa/crypto/utils/Utils.kt
+++ app/src/main/java/com/engineerskasa/crypto/utils/Utils.kt
@@ -3,4 +3,6 @@ package com.engineerskasa.crypto.utils
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import j
+import javax.inject.Inject
+
+class Utils @Inject constructor(private v
