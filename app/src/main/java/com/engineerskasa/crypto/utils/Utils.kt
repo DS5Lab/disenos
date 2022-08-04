5e7c4fd17b9e579f0ae3ff53ccd20c97d42ae265
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-08-04 15:45:43 +0000

    fix typo last commit

--- app/src/main/java/com/engineerskasa/crypto/utils/Utils.kt
+++ app/src/main/java/com/engineerskasa/crypto/utils/Utils.kt
@@ -8,4 +8,4 @@ import javax.inject.Inject
 class Utils @Inject constructor(private val context: Context) {
 
     fun isConnectedToInternet() =
- 
+        (context.getSystemService(Context.CONNECTIVITY_SE
