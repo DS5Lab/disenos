47d44511d08d2440cc524d5bd5b0e38aefb824be
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2022-12-04 17:35:29 +0000

    [ENGOPS-540] shared file update from distribute.groovy

--- app/src/test/java/com/engineerskasa/crypto/ExampleUnitTest.kt
+++ app/src/test/java/com/engineerskasa/crypto/ExampleUnitTest.kt
@@ -7,4 +7,8 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fu
