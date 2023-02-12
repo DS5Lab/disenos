4cc7a8db7a867257e14aed187056ee71664579f8
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2023-02-12 18:54:00 +0000

    refactor

--- gradlew.bat
+++ gradlew.bat
@@ -6,4 +6,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"==
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
