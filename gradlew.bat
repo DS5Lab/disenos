d3e2c9e10148b11518d5d36e5df85b3a14726019
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2023-02-21 22:36:00 +0000

    wire module

--- gradlew.bat
+++ gradlew.bat
@@ -24,4 +24,6 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' com
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set
