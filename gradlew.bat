a8b96fe35c4b631e00b1ee6a00c17b129b5e4708
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2023-02-24 06:44:05 +0000

    Added gradle wrapper

--- gradlew.bat
+++ gradlew.bat
@@ -27,4 +27,8 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-ech
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaH
