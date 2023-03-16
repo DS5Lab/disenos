3a38441da7989fa8a761be1e245d0db6d7bf8956
DS5Lab <96418248+DS5Lab@users.noreply.github.com>
2023-03-16 14:27:54 +0000

    Layer

--- gradlew.bat
+++ gradlew.bat
@@ -72,4 +72,5 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
-:f
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if y
